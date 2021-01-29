b520d86dc2b30113bfb65abe6aea29aab1b64bb2
rob <rob>
2021-01-29 01:28:26 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Roberto Sanchez, sanc735@usc.edu
+# ITP 115, Spring 2021
+# Assignment 1
+# Description:
+# This program displays two truths and a lie.
+
+first = "Roberto"
+last = "Sanchez"
+statement1 = "I don't have my driver's license."
+statement2 = "I love gambling."
+statement3 = "I've been to Disneyland five times."
+truth1 = False
+truth2 = True
+truth3 = True
+pets = 1
+siblings = 2
+
+print("\nFull name: {} {}".format(first, last))  # prints formatted string with full name
+
+print("\nNumber of pets: {}".format(pets))  # prints formatted string with num of pets
+print("Number of siblings: {}".format(siblings))  # prints formatted string with num of siblings
+
+print("\nStatement 1: {}".format(statement1))  # prints formatted string with statement 1
+print("Statement 2: {}".format(statement2))  # prints formatted string with statement 2
+print("Statement 3: {}".format(statement3))  # prints formatted string with statement 3
+
+print("\nStatement 1: {}".format(truth1))  # prints formatted string with truth 1
+print("Statement 2: {}".format(truth2))  # prints formatted string with truth 2
+print("Statement 3: {}".format(truth3))  # prints formatted string with truth 3
